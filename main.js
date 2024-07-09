cf5904de74b7b86317922885ea011d1fc60491cb
Saba Nizam <160767347+SabaNizam90@users.noreply.github.com>
2024-07-10 00:00:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//Author : [Saba Nizam]
+//Date: [21-05-2024]
+//task 9 : printing my favourite number
+var favouriteNumber = 10;
+//revealing my favourite number in a message format.
+console.log("My favourite number is ".concat(favouriteNumber, "."));
